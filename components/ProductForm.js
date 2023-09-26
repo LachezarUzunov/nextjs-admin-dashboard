463d0a149ced8651b3526bade8783f8d545c6ad9
Lachezar <uzunov_89_lacho@abv.bg>
2023-09-26 17:26:01 +0300

    build:upload file input

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice }) {
+export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice, images }) {
     const router = useRouter();
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
@@ -38,7 +38,20 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
                 value={title}
                 onChange={ev => setTitle(ev.target.value)}
             />
-            <lable>Description</lable>
+            <label>Photos</label>
+            <div className="mb-2">
+                <label className="w-24 h-24 text-center flex flex-col items-center justify-center rounded-lg bg-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                </svg>
+                    <div>Upload</div>
+                <input type="file" className="hidden"/>
+                </label>
+                {!images?.length && (
+                    <div>No photos in this product</div>
+                )}
+            </div>
+            <label>Description</label>
             <textarea 
                 placeholder="description"
                 value={description}
